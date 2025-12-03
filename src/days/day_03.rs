d0835eb5782d172ad58e32004dd2008806b70e6c
Emanuele DelBono <emanuele@codiceplastico.com>
2025-12-03 16:14:27 +0100

    day 3

--- src/days/day_03.rs
+++ src/days/day_03.rs
@@ -1,5 +1,3 @@
-use std::arch::aarch64::uint64x2x3_t;
-
 fn parse(input: String) -> Vec<Vec<u32>> {
     let moves: Vec<Vec<u32>> = input
         .lines()
@@ -9,12 +7,11 @@ fn parse(input: String) -> Vec<Vec<u32>> {
     moves
 }
 
-fn find_max(nums: &Vec<u32>, start: usize, end: usize) -> (u32, usize) {
-    // println!(">> {} {} {:?}", start, end, nums);
+fn find_max(nums: &Vec<u32>, end: usize) -> (u32, usize) {
     let mut max = nums[0];
     let mut idx = 0;
-    for i in (start + 1)..end {
-        // println!("@ {} {}", nums[i], max);
+
+    for i in 1..end {
         if nums[i] > max {
             max = nums[i];
             idx = i;
@@ -23,69 +20,27 @@ fn find_max(nums: &Vec<u32>, start: usize, end: usize) -> (u32, usize) {
     (max, idx)
 }
 
-pub fn part_1(input: String) -> u32 {
-    let nums = parse(input);
-    let mut sum = 0;
-    for n in nums {
-        let (max1, idx) = find_max(&n, 0, n.len() - 1);
-        // println!("max={} idx={}", max1, idx);
-        let new_vec = &n[(idx + 1)..].to_vec();
+fn find_n_digits(line: Vec<u32>, digits: usize) -> u64 {
+    let (_, number) = (0..digits).rev().fold((line, 0 as u64), |(list, sum), i| {
+        let (max, idx) = find_max(&list, list.len() - i);
+        let new_vec = list[(idx + 1)..].to_vec().clone();
+        let new_acc: u64 = sum + (max as u64) * (10 as u64).pow(i as u32);
 
-        let (max2, _) = find_max(new_vec, 0, new_vec.len());
-        println!("{} {} ==> {}", max1, max2, (max1 * 10) + max2);
-        //
-        sum = sum + (max1 * 10) + max2;
-    }
+        (new_vec, new_acc)
+    });
+    number
+}
 
-    sum
+pub fn part_1(input: String) -> u64 {
+    let lines = parse(input);
+    lines
+        .into_iter()
+        .fold(0, |acc, n| acc + find_n_digits(n, 2))
 }
 
 pub fn part_2(input: String) -> u64 {
-    let nums = parse(input);
-    let mut sum: u64 = 0;
-    for n in nums {
-        let (m1, idx) = find_max(&n, 0, n.len() - 11);
-        let new_vec = &n[(idx + 1)..].to_vec();
-
-        let (m2, idx) = find_max(new_vec, 0, new_vec.len() - 10);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m3, idx) = find_max(new_vec, 0, new_vec.len() - 9);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m4, idx) = find_max(new_vec, 0, new_vec.len() - 8);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m5, idx) = find_max(new_vec, 0, new_vec.len() - 7);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m6, idx) = find_max(new_vec, 0, new_vec.len() - 6);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m7, idx) = find_max(new_vec, 0, new_vec.len() - 5);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m8, idx) = find_max(new_vec, 0, new_vec.len() - 4);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m9, idx) = find_max(new_vec, 0, new_vec.len() - 3);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m10, idx) = find_max(new_vec, 0, new_vec.len() - 2);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m11, idx) = find_max(new_vec, 0, new_vec.len() - 1);
-        let new_vec = &new_vec[(idx + 1)..].to_vec();
-
-        let (m12, _) = find_max(new_vec, 0, new_vec.len());
-
-        let str = format!(
-            "{}{}{}{}{}{}{}{}{}{}{}{}",
-            m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12
-        );
-
-        sum = sum + str.parse::<u64>().unwrap();
-    }
-
-    sum
+    let lines = parse(input);
+    lines
+        .into_iter()
+        .fold(0, |acc, n| acc + find_n_digits(n, 12))
 }
